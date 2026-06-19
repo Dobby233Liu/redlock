d5cc364d13cdb9b53d6bb5d7ab06be87be53f6ab
Liu Wenyuan <15816141883@163.com>
2026-06-19 23:29:20 +0800

    the

--- redlock/Program.Unlock.cs
+++ redlock/Program.Unlock.cs
@@ -346,145 +346,149 @@ internal static partial class Program
 
 	private static int GetRequiredRPVersion(string filePath)
 	{
-		var num = int.MaxValue;
-		using var fileStream = new FileStream(filePath, FileMode.Open);
-		using var binaryReader = new BinaryReader(fileStream);
-		binaryReader.BaseStream.Seek(60L, SeekOrigin.Begin);
-		binaryReader.BaseStream.Seek(binaryReader.ReadInt32() + 4, SeekOrigin.Begin);
-		var num2 = binaryReader.ReadUInt16();
-		var flag = false;
+		var result = int.MaxValue;
+		
+		using var stream = new FileStream(filePath, FileMode.Open);
+		using var reader = new BinaryReader(stream);
+		
+		reader.BaseStream.Seek(0x3CL, SeekOrigin.Begin);
+		reader.BaseStream.Seek(reader.ReadInt32() + 4, SeekOrigin.Begin);
+		
+		var archId = reader.ReadUInt16();
+		const int archX86 = 0x14C, archAmd64 = 0x8664;
+		var isAmd64 = false;
 		Console.Write(" -> Architecture: ");
-		if (num2 == 34404)
+		switch (archId)
 		{
-			Console.WriteLine("x64");
-			flag = true;
-		}
-		else
-		{
-			if (num2 != 332)
-			{
-				Console.WriteLine("Unknown");
-				return num;
-			}
-
+		case archX86:
 			Console.WriteLine("x86");
+			break;
+		case archAmd64:
+			Console.WriteLine("x64");
+			isAmd64 = true;
+			break;
+		default:
+			Console.WriteLine("Unknown");
+			return result;
 		}
 
-		var num3 = binaryReader.ReadUInt16();
-		binaryReader.BaseStream.Seek(flag ? 40L : 44L, SeekOrigin.Current);
-		var num4 = flag ? binaryReader.ReadInt64() : binaryReader.ReadInt32();
-		binaryReader.BaseStream.Seek(flag ? 208L : 192L, SeekOrigin.Current);
-		var list = new List<PESectionInfo>();
-		var num5 = 0;
-		for (var i = 0; i < num3; i++)
+		var sectionCount = reader.ReadUInt16();
+		reader.BaseStream.Seek(isAmd64 ? 0x28L : 0x2CL, SeekOrigin.Current);
+		var virtAddrOfsMaybe = isAmd64 ? reader.ReadInt64() : reader.ReadInt32();
+		reader.BaseStream.Seek(isAmd64 ? 0xD0L : 0xC0L, SeekOrigin.Current);
+		
+		var sections = new List<PESectionInfo>();
+		var rsrcAddr = 0;
+		for (var i = 0; i < sectionCount; i++)
 		{
-			var pesectionInfo = new PESectionInfo
+			var section = new PESectionInfo
 			{
-				SectionName = Encoding.ASCII.GetString(binaryReader.ReadBytes(8)).TrimEnd(new char[1]),
-				VirtSize = binaryReader.ReadInt32(),
-				VirtAddr = binaryReader.ReadInt32(),
-				PhysSize = binaryReader.ReadInt32(),
-				PhysAddr = binaryReader.ReadInt32()
+				SectionName = Encoding.ASCII.GetString(reader.ReadBytes(8)).TrimEnd(new char[1]),
+				VirtSize = reader.ReadInt32(),
+				VirtAddr = reader.ReadInt32(),
+				PhysSize = reader.ReadInt32(),
+				PhysAddr = reader.ReadInt32()
 			};
-			if (num5 < 1 && pesectionInfo.SectionName == ".rsrc") num5 = pesectionInfo.PhysAddr;
-			pesectionInfo.VirtOffset = pesectionInfo.VirtAddr - pesectionInfo.PhysAddr;
-			list.Add(pesectionInfo);
-			binaryReader.BaseStream.Seek(16L, SeekOrigin.Current);
+			section.VirtOffset = section.VirtAddr - section.PhysAddr;
+			
+			sections.Add(section);
+			if (rsrcAddr < 1 && section.SectionName == ".rsrc") rsrcAddr = section.PhysAddr;
+			reader.BaseStream.Seek(16L, SeekOrigin.Current);
 		}
 
-		var stringPhysAddr = PatternFinder.FindPattern(binaryReader, Encoding.ASCII.GetBytes("RP_VersionCheck"),
-			true, list[0].PhysAddr, num5);
-		if (stringPhysAddr > -1L)
+		var verCheckDefStart = PatternFinder.FindPattern(reader, Encoding.ASCII.GetBytes("RP_VersionCheck"),
+			true, sections[0].PhysAddr, rsrcAddr);
+		if (verCheckDefStart <= -1L)
 		{
-			var pesectionInfo2 = list.First(x =>
-				stringPhysAddr > x.PhysAddr && stringPhysAddr < x.PhysAddr + x.PhysSize);
-			var num6 = stringPhysAddr + pesectionInfo2.VirtOffset;
-			Console.WriteLine(" -> Found RP_VersionCheck at 0x{0:x} (virtual address 0x{1:x} in {2})",
-				stringPhysAddr, num6, pesectionInfo2.SectionName);
-			binaryReader.BaseStream.Seek(list[0].PhysAddr, SeekOrigin.Begin);
-			var array = new byte[15];
-			if (flag)
+			Console.WriteLine(" -> TWinUI doesn't contain RP_VersionCheck");
+			return result;
+		}
+		
+		var verCheckSection = sections.First(x =>
+			verCheckDefStart > x.PhysAddr && verCheckDefStart < x.PhysAddr + x.PhysSize);
+		var verCheckVirtAddr = verCheckDefStart + verCheckSection.VirtOffset;
+		Console.WriteLine(" -> Found RP_VersionCheck at 0x{0:x} (virtual address 0x{1:x} in {2})",
+			verCheckDefStart, verCheckVirtAddr, verCheckSection.SectionName);
+		reader.BaseStream.Seek(sections[0].PhysAddr, SeekOrigin.Begin);
+
+		var buf = new byte[15];
+		if (!isAmd64)
+		{
+			verCheckVirtAddr += virtAddrOfsMaybe;
+
+			var findFail = false;
+			while (buf[10] != 0x68 || BitConverter.ToInt32(buf, 11) != verCheckVirtAddr)
 			{
-				var flag2 = false;
-				for (;;)
-				{
-					if (array[8] != 72 || array[9] != 141 || array[10] != 21)
-					{
-						Array.Copy(array, 1, array, 0, 14);
-						try
-						{
-							array[array.Length - 1] = binaryReader.ReadByte();
-						}
-						catch
-						{
-							flag2 = true;
-							goto IL_02BA;
-						}
-
-						continue;
-					}
-
-					IL_02BA:
-					if (flag2) goto IL_0398;
-					var num7 = (ulong)(num6 - (binaryReader.BaseStream.Position + list[0].VirtOffset)) &
-					           0xFFFFFFFFFFFFFFFF;
-					var num8 = (uint)BitConverter.ToInt32(array, 11);
-					if (num7 == num8) break;
-					array[8] = 0;
-				}
-
-				Console.WriteLine(" -> Found matching lea rdx");
-			}
-			else
-			{
-				num6 += num4;
-				var flag3 = false;
-				while (array[10] != 104 || BitConverter.ToInt32(array, 11) != num6)
-				{
-					Array.Copy(array, 1, array, 0, 14);
-					try
-					{
-						array[array.Length - 1] = binaryReader.ReadByte();
-					}
-					catch
-					{
-						flag3 = true;
-						break;
-					}
-				}
-
-				if (!flag3)
-					Console.WriteLine(" -> Found matching push offset at 0x{0:x}",
-						binaryReader.BaseStream.Position - 5L);
-			}
-
-			IL_0398:
-			while ((array[10] != 131 || array[11] != 248) &&
-			       (array[10] != 61 || array[13] != 1 || array[14] != 0))
-			{
-				Array.Copy(array, 1, array, 0, 14);
+				Array.Copy(buf, 1, buf, 0, 14);
 				try
 				{
-					array[array.Length - 1] = binaryReader.ReadByte();
+					buf[buf.Length - 1] = reader.ReadByte();
 				}
 				catch
 				{
-					return num;
+					findFail = true;
+					break;
 				}
 			}
 
-			if (array[14] == 0)
-				num = BitConverter.ToInt32(array, 11);
-			else
-				num = array[12];
-			Console.WriteLine(" -> Found cmp eax, {0:x} at 0x{1:x}", num,
-				(int)binaryReader.BaseStream.Position - 5);
+			if (!findFail)
+				Console.WriteLine(" -> Found matching push offset at 0x{0:x}",
+					reader.BaseStream.Position - 5L);
 		}
 		else
 		{
-			Console.WriteLine(" -> TWinUI doesn't contain RP_VersionCheck");
+			var findFail = false;
+			while (true)
+			{
+				if (buf[8] != 0x48 || buf[9] != 0x8D || buf[10] != 0x15)
+				{
+					Array.Copy(buf, 1, buf, 0, 14);
+					try
+					{
+						buf[buf.Length - 1] = reader.ReadByte();
+					}
+					catch
+					{
+						findFail = true;
+						goto skipContinue;
+					}
+
+					continue;
+				}
+
+				skipContinue:
+				if (findFail) goto findCmp;
+				var num7 = (ulong)(verCheckVirtAddr - (reader.BaseStream.Position + sections[0].VirtOffset));
+				num7 &= 0xFFFFFFFFFFFFFFFF;
+				var num8 = (uint)BitConverter.ToInt32(buf, 11);
+				if (num7 == num8) break;
+				buf[8] = 0;
+			}
+
+			Console.WriteLine(" -> Found matching lea rdx");
 		}
 
-		return num;
+		findCmp:
+		while ((buf[10] != 0x83 || buf[11] != 0xF8) &&
+		       (buf[10] != 0x3D || buf[13] != 0x1 || buf[14] != 0x0))
+		{
+			Array.Copy(buf, 1, buf, 0, 14);
+			try
+			{
+				buf[buf.Length - 1] = reader.ReadByte();
+			}
+			catch
+			{
+				return result;
+			}
+		}
+		if (buf[14] == 0)
+			result = BitConverter.ToInt32(buf, 11);
+		else
+			result = buf[12];
+		Console.WriteLine(" -> Found cmp eax, {0:x} at 0x{1:x}", result,
+			(int)reader.BaseStream.Position - 5);
+
+		return result;
 	}
 }
