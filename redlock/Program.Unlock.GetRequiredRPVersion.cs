4768979ffa1d679671ff6567070001f21cfd18ad
Liu Wenyuan <15816141883@163.com>
2026-06-21 17:45:55 +0800

    remove use of peimage & cont if cmp find fails

--- redlock/Program.Unlock.GetRequiredRPVersion.cs
+++ redlock/Program.Unlock.GetRequiredRPVersion.cs
@@ -16,10 +16,11 @@ internal partial class Program
 	private static int GetRequiredRPVersion(string tWinUiPath)
 	{
 		var file = PEFile.FromFile(tWinUiPath);
-		var image = PEImage.FromFile(file);
+		var machineType = file.FileHeader.Machine;
+		ulong RvaToVa(ulong rva) => file.OptionalHeader.ImageBase + rva;
 		
-		Console.Write($" -> TWinUI architecture: {image.MachineType.ToString()}");
-		if (image.MachineType is not (MachineType.I386 or MachineType.Amd64))
+		Console.Write($" -> TWinUI architecture: {machineType.ToString()}");
+		if (machineType is not (MachineType.I386 or MachineType.Amd64))
 		{
 			Console.WriteLine(" (unsupported)");
 			return int.MaxValue;
@@ -39,17 +40,18 @@ internal partial class Program
 		var verCheckAddr = (ulong)verCheckAddrS;
 		var verCheckRva = file.FileOffsetToRva(verCheckAddr);
 		var verCheckSection = file.GetSectionContainingRva(verCheckRva); 
-		var verCheckVa = image.ImageBase + verCheckRva;
+		var verCheckVa = RvaToVa(verCheckRva);
 		Console.WriteLine($" -> Found {RpVersionCheckStr} in {verCheckSection.Name} at 0x{verCheckAddr:x}"
 							+ $" (VA 0x{verCheckVa:x})");
 
 		var codeSectionData = codeSection.ToArray();
-		var codeSectionRva = image.ImageBase + codeSection.Rva;
+		var codeSectionRva = RvaToVa(codeSection.Rva);
 
-		return image.MachineType switch
+		return machineType switch
 		{
 			MachineType.I386 => X86FindAddrLoad(codeSectionData, verCheckVa),
 			MachineType.Amd64 => Amd64FindAddrLoad(codeSectionData, codeSectionRva, verCheckVa),
+			// ReSharper disable once UnreachableSwitchArmDueToIntegerAnalysis
 			_ => int.MaxValue
 		};
 	}
@@ -63,13 +65,17 @@ internal partial class Program
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
 			bool BytesEnough(int count) => i - 1 + count < code.Length;
 
-			// push imm32 => 68 xx xx xx xx
-			if (BytesEnough(5 + 1) && code[i] == 0x68)
+			// push imm32
+			if (BytesEnough(5 + 1)
+			    && code[i] == 0x68)
 			{
 				if (BitConverter.ToUInt32(code, i + 1) != targetVa)
 					continue;
 				Console.WriteLine($" -> Found push 0x{targetVa:x4} at 0x{i:x}");
-				return X86FindCmp(code, i + 4 + 1, 20);
+				
+				var result = X86FindCmp(code, i + 4 + 1, 20);
+				if (result != int.MaxValue)
+					return result;
 			}
 		}
 		return int.MaxValue;
@@ -82,8 +88,9 @@ internal partial class Program
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
 			bool BytesEnough(int count) => i - 1 + count < code.Length;
 			
-			// lea rdx, [rip + disp32] => 48 8D 15 xx xx xx xx
-			if (BytesEnough(7 + 1) && code[i] == 0x48 && code[i + 1] == 0x8D && code[i + 2] == 0x15)
+			// lea rdx, [rip + disp32]
+			if (BytesEnough(7 + 1)
+			    && code[i] == 0x48 && code[i + 1] == 0x8D && code[i + 2] == 0x15)
 			{
 				// Calculate what RIP-relative address this would reference
 				var displacement = BitConverter.ToUInt32(code, i + 3);
@@ -91,7 +98,10 @@ internal partial class Program
 				if (loadingAddr != targetVa)
 					continue;
 				Console.WriteLine($" -> Found lea rdx, 0x{loadingAddr:x4} at 0x{i:x}");
-				return X86FindCmp(code, i + 6 + 1, 20);
+				
+				var result = X86FindCmp(code, i + 6 + 1, 20);
+				if (result != int.MaxValue)
+					return result;
 			}
 		}
 		return int.MaxValue;
@@ -105,18 +115,21 @@ internal partial class Program
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
 			bool BytesEnough(int count) => i - 1 + count < code.Length;
 			
-			// cmp eax, imm8 => 83 F8 xx
-			if (BytesEnough(3) && code[i] == 0x83 && code[i + 1] == 0xF8)
+			// cmp eax, imm8
+			if (BytesEnough(3)
+			    && code[i] == 0x83 && code[i + 1] == 0xF8)
 			{
 				var result = code[i + 2];
 				Console.WriteLine($" -> Found cmp eax, 0x{result:x4} at 0x{i:x}");
 				return result;
 			}
 
-			// cmp eax, imm32 => 3D xx xx xx xx
-			if (BytesEnough(5) && code[i] == 0x3D)
+			// cmp eax, imm32
+			if (BytesEnough(5)
+			    && code[i] == 0x3D)
 			{
-				if (!(code[i + 3] == 0x01 && code[i + 4] == 0x00)) // result < 0x100 || result >= 0x200
+				// result < 0x100 || result >= 0x200
+				if (!(code[i + 3] == 0x01 && code[i + 4] == 0x00))
 					continue;
 				var result = BitConverter.ToInt32(code, i + 1);
 				Console.WriteLine($" -> Found cmp eax, 0x{result:x4} at 0x{i:x}");
